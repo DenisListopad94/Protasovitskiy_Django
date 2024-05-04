b32a7eadb0f26a09cfd2559754ba9f42338074c9
Resmor <strazhev90@list.ru>
2024-05-04 14:44:56 +0300

    Hometask 23 Django_3

--- src/booking_app/models.py
+++ src/booking_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Persons(models.Model):
+    SEX_PERSON = {
+        "m": "male",
+        "f": "female",
+    }
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    sex = models.CharField(max_length=1, choices=SEX_PERSON)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f" {self.first_name} {self.last_name}"
+
+
+class Hotels(models.Model):
+    name = models.CharField(max_length=40)
+    stars = models.IntegerField(null=True)
+    address = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    phone = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f" {self.name} {self.address}"
